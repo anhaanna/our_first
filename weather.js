f8c469e93001a1b9cd13bf38887d8ce5630ddb60
anhaanna <annaha1995@mail.ru>
2017-10-03 20:15:16 +0400

    files

---
+++ weather.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	$('#city').on('input', function(){
+		var city = $('#city').val();
+		 if (city != "") {
+		 	$.ajax({
+		 		url:"http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" +
+		 		 "&APPID=7326349b9a064839e6fdb3533df3902d", 
+		 		type: "GET",
+		 		dataType: "jsonp",
+		 		success: function(data){
+		 			// console.log(data);
+		 			var file = show(data);
+		 			$('#show').html(file);
+		 			//$('#city').val('');
+		 			$('#error').html('');
+		 		}
+		 	});
+		 }
+		 else {
+		 	$('#error').html('Error! Field is empty.')
+		 }
+	});
+
+});
+
+ function show(data){
+ 	return "<h2>Current Weather for "+ data.name +", "+ data.sys.country + "</h2>" +
+ 		   "<h3><b>Weather</b>: "+ data.weather[0].main +" </h3>" +
+ 		   "<h3><b>Description</b>: <img src='http://openweathermap.org/img/w/"+ data.weather[0].icon +".png'> "+ data.weather[0].description +" </h3>" +
+ 		   "<h3><b>Temperatue</b>: "+ data.main.temp +"&deg;C  </h3>" +
+ 		   "<h3><b>Pressure</b>: "+ data.main.pressure +" </h3>" +
+ 		   "<h3><b>Humidity</b>: "+ data.main.humidity +" </h3>" +
+ 		   "<h3><b>Min_temperatue</b>: "+ data.main.temp_min +"&deg;C </h3>" +
+ 		   "<h3><b>Max_temperatue</b>: "+ data.main.temp_max +"&deg;C </h3>" +
+ 		   "<h3><b>Wind Speed</b>: "+ data.wind.speed +" m/s </h3>" ;
+
+ 		   
+
+
+ }
+
+
+
+
+
+
+
